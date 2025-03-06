9df8ac7c4c09356b9097d446af06bd4629bccd87
Lilithe Bowman <lilithe.bowman@gmail.com>
2025-03-06 11:31:30 -0500

    Setting up Typescript

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-import "./index.css";
-import App from "./App";
+import './index.css'
+import App from './App'
 
-const queryClient = new QueryClient();
 
-ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
-  document.getElementById("root"),
-);
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+const queryClient = new QueryClient()
+
+root.render(
+	<QueryClientProvider client={queryClient} >
+		<App />
+	</QueryClientProvider>,
+	document.getElementById('root'),
+)
